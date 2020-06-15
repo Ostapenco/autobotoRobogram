d38ce12e74d9abb80b326f348d33f649694d70c0
Eugene_Ostapenco <evgheni@mediapark.com>
2020-06-15 13:05:48 +0300

    Updates

---
+++ precache-manifest.f410b61a2f975e4d6496297c4e876704.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07e7d168142ace3408d8e6cb6abae76",
+    "url": "/autobotoRobogram/index.html"
+  },
+  {
+    "revision": "112d837acc0741831ed8",
+    "url": "/autobotoRobogram/static/css/main.31f5cc3d.chunk.css"
+  },
+  {
+    "revision": "a67c8bdfa56f9aee65e3",
+    "url": "/autobotoRobogram/static/js/2.ceb702c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/autobotoRobogram/static/js/2.ceb702c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112d837acc0741831ed8",
+    "url": "/autobotoRobogram/static/js/main.4401f43b.chunk.js"
+  },
+  {
+    "revision": "c1f0602340421c66167e",
+    "url": "/autobotoRobogram/static/js/runtime-main.c77aa74e.js"
+  },
+  {
+    "revision": "b14011e51deed67ca22a019976a4b014",
+    "url": "/autobotoRobogram/static/media/autobot-logo.b14011e5.svg"
+  },
+  {
+    "revision": "7dbaa9f8e1f82a01890a450faf5c34e0",
+    "url": "/autobotoRobogram/static/media/compass.7dbaa9f8.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/autobotoRobogram/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "906321b3296533cb7b907c36cb19c2a8",
+    "url": "/autobotoRobogram/static/media/instagram-logo.906321b3.svg"
+  },
+  {
+    "revision": "693576e2ce60122176044fd97545fbbd",
+    "url": "/autobotoRobogram/static/media/profileIcon.693576e2.svg"
+  },
+  {
+    "revision": "dd9b4cef4c51baa038e950f3900f2aa6",
+    "url": "/autobotoRobogram/static/media/search.dd9b4cef.svg"
+  }
+]);
