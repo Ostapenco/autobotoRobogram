a4bec8891bf4bd0cbfb2d707355ef9db9b9e8562
Eugene_Ostapenco <evgheni@mediapark.com>
2020-06-15 11:55:52 +0300

    add build folder

---
+++ build/precache-manifest.df6d8328663e476c00d283ecc5cac620.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c07929e15899a8bcbb7a54dcfc9488",
+    "url": "/index.html"
+  },
+  {
+    "revision": "627be121e56654bf100b",
+    "url": "/static/css/main.31f5cc3d.chunk.css"
+  },
+  {
+    "revision": "a67c8bdfa56f9aee65e3",
+    "url": "/static/js/2.ceb702c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ceb702c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627be121e56654bf100b",
+    "url": "/static/js/main.bf6d237b.chunk.js"
+  },
+  {
+    "revision": "310b9bc6906bcb248e8d",
+    "url": "/static/js/runtime-main.45cd8038.js"
+  },
+  {
+    "revision": "b14011e51deed67ca22a019976a4b014",
+    "url": "/static/media/autobot-logo.b14011e5.svg"
+  },
+  {
+    "revision": "7dbaa9f8e1f82a01890a450faf5c34e0",
+    "url": "/static/media/compass.7dbaa9f8.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "906321b3296533cb7b907c36cb19c2a8",
+    "url": "/static/media/instagram-logo.906321b3.svg"
+  },
+  {
+    "revision": "693576e2ce60122176044fd97545fbbd",
+    "url": "/static/media/profileIcon.693576e2.svg"
+  },
+  {
+    "revision": "dd9b4cef4c51baa038e950f3900f2aa6",
+    "url": "/static/media/search.dd9b4cef.svg"
+  }
+]);
